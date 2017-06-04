5e0f0c62c0f2e5b8fa138b4d8319f1466c770e9e
Aleksandr Shchigrintsov <aleksrullz@gmail.com>
2017-06-04 12:08:54 +0300

    Signin, signup, signout component with valdiation for signup

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 
+import * as actions from '../../actions';
+
 class Signin extends Component {
 
   handleFormSubmit = ({ email, password }) => {
-    console.log(email, password);
+    this.props.signin({ email, password });
+  }
+
+  renderAlert = () => {
+    if (this.props.errorMessage) {
+      return (
+        <div className="alert alert-danger">
+          <strong>Oops!</strong> {this.props.errorMessage}
+        </div>
+      );
+    }
   }
 
   render() {
@@ -18,15 +30,20 @@ class Signin extends Component {
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
-          <input {...password} className="form-control" />
+          <input {...password} type="password" className="form-control" />
         </fieldset>
+        {this.renderAlert()}
         <button action="submit" className="btn btn-primary">Sign In</button>
       </form>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.error };
+}
+
 export default reduxForm({
   form: 'signin',
   fields: ['email', 'password']
-})(Signin);
+}, mapStateToProps, actions)(Signin);
--- src/index.js
+++ src/index.js
@@ -3,19 +3,24 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
 import Signin from './components/auth/signin';
+import Signup from './components/auth/signup';
+import Signout from './components/auth/signout';
 
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
-        <Route path="/signin" component={Signin} />
+        <Route path="signin" component={Signin} />
+        <Route path="signup" component={Signup} />
+        <Route path="signout" component={Signout} />
       </Route>
     </Router>
   </Provider>
